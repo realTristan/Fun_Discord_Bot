0751a980187342cd39a49c6c35df3afd7cfc8168
Tristan <75189508+realTristan@users.noreply.github.com>
2021-08-10 09:53:14 -0400

    Create settings.py

---
+++ cogs/settings.py
@@ -0,0 +1,42 @@
+import discord, os, os.path, json, asyncio, random
+from discord.ext import commands
+from discord.utils import get, find
+from discord.ext.commands import has_permissions
+import datetime as datetime
+from discord_components import *
+
+
+class Settings(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+
+    def write(self, file, data, f):
+        with open(os.path.dirname(__file__) + f'\\..\\json\\{file}.json', 'w') as f:
+            json.dump(data, f, indent=4)
+            f.close()
+
+
+    @commands.command()
+    async def setup(self, ctx):
+        with open(os.path.dirname(__file__) + '\\..\\json\\data.json','r+') as f:
+            data=json.load(f)
+            if str(ctx.message.guild.id) not in data:
+                data.update({
+                    "commands" : {
+                        "kill_command": {
+                            "kill_timer": 0
+                        }
+                    },
+                    "users" : {
+
+                    }
+                })
+
+        self.write('data', data, f)
+
+
+
+        
+def setup(client):
+    client.add_cog(Settings(client))
