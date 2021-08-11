f4b54f919ed5e01b795bbebfa30b236ce0bb5056
Tristan <75189508+realTristan@users.noreply.github.com>
2021-08-11 09:40:23 -0400

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,4 +1,4 @@
-import discord, os, os.path, json, asyncio, random
+import discord, os, os.path, json, asyncio, random, string
 from discord.ext import commands
 from discord.utils import get, find
 from discord.ext.commands import has_permissions
@@ -9,6 +9,53 @@ from discord_components import *
 class Events(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    def write(self, file, data, f):
+        with open(os.path.dirname(__file__) + f'\\..\\json\\{file}.json', 'w') as x:
+            json.dump(data, x, indent=4)
+            x.close()
+
+
+
+
+    @commands.command()
+    @has_permissions(administrator=True)
+    async def addQAevent(self, ctx, args):
+        qa = str(args).split(":")
+
+        with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
+            data=json.load(f)
+            data[str(ctx.message.guild.id)]["qa_event"].update({qa[0]: qa[1]})
+            self.write("events", data, f)
+
+        await ctx.send(embed=discord.Embed(title='Q&A Event Added', desctipyion=f'**Question:** {qa[0]}\n**Answer:** {qa[1]}', color=65535))
+
+
+
+
+
+    @commands.command()
+    @has_permissions(manage_messages=True)
+    async def startQAevent(self, ctx):
+        id = ''.join(random.choice(string.digits) for _ in range(3))
+        with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
+            data=json.load(f)
+            events = list(data[str(ctx.message.guild.id)]["qa_event"])
+            q = random.choice(events)
+            await ctx.send(embed=discord.Embed(title=f'Q&A Event #{id}', description=f'{q}', color=65535))
+        try:
+            msg = await self.client.wait_for("message", check=lambda m: m.channel == ctx.message.channel, timeout=300)
+            if data[str(ctx.message.guild.id)]["qa_event"][str(q)] in msg.content:
+                await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} was correct! +20 points', color=65535))
+                with open(os.path.dirname(__file__) + '\\..\\json\\data.json','r+') as f:
+                    data=json.load(f)
+                    data[str(msg.guild.id)]["users"][str(msg.author.id)]["points"] += 20
+                    self.write("data", data, f)
+
+        except asyncio.TimeoutError:
+            await ctx.send(embed=discord.Embed(title=f'Q&A Event #{id}', description=f'**Correct answer:** {data[str(ctx.message.guild.id)]["qa_event"][str(q)]}', color=65535))
+
+
 
 
 
