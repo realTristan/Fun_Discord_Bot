f5555d22a2e977a6a0042adc860984af19bdcb2c
Tristan <75189508+realTristan@users.noreply.github.com>
2021-08-13 09:21:36 -0400

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -18,79 +18,61 @@ class Events(commands.Cog):
 
 
     @commands.command()
-    @has_permissions(administrator=True)
-    async def addQAevent(self, ctx, *args):
-        string = ' '.join(str(enum) for enum in list(args))
-        qa = string.split(">")
-        with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
-            data=json.load(f)
-            data[str(ctx.message.guild.id)]["qa_event"].update({qa[0]: qa[1]})
-            self.write("events", data, f)
-
-        await ctx.send(embed=discord.Embed(title='Q&A Added', description=f'**Question:** {qa[0]}\n**Answer:** {qa[1]}', color=65535))
-
+    async def event(self, ctx, choice: str, *args):
+        if choice == 'add':
+            stuff = ' '.join(str(enum) for enum in list(args))
+            qa = stuff.split(">")
+            with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
+                data=json.load(f)
+                data[str(ctx.message.guild.id)]["qa_event"].update({qa[0]: qa[1]})
+                self.write("events", data, f)
+            await ctx.send(embed=discord.Embed(title='Q&A Added', description=f'**Question:** {qa[0]}\n**Answer:** {qa[1]}', color=65535))
 
 
-    @commands.command()
-    @has_permissions(administrator=True)
-    async def delQAevent(self, ctx, event: str):
-        with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
-            data=json.load(f)
-            for line in list(data[str(ctx.message.guild.id)]["qa_event"]):
-                    if event in line:
+        if choice == 'del':
+            with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
+                data=json.load(f)
+                await ctx.send(embed=discord.Embed(title='Q&A Removed', description=f'**Question:** {list(args)[0]}\n**Answer:** {data[str(ctx.message.guild.id)]["qa_event"][str(list(args)[0])]}', color=65535))
+                for line in list(data[str(ctx.message.guild.id)]["qa_event"]):
+                    if list(args)[0] in line:
                         del data[str(ctx.message.guild.id)]["qa_event"][str(line)]
-            self.write("events", data, f)
-        await ctx.send(embed=discord.Embed(title='Q&A Removed', description=f'**Question:** {event}\n**Answer:** {data[str(ctx.message.guild.id)]["qa_event"][str(event)]}', color=65535))
+                self.write("events", data, f)
 
 
-
-    @commands.command()
-    @has_permissions(manage_messages=True)
-    async def showQAevents(self, ctx):
-        with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
-            data=json.load(f)
-            embed=discord.Embed(title='Q&A Events', color=65535)
-            for line in data[str(ctx.message.guild.id)]["qa_event"]:
-                embed.add_field(name=f'{line}', value=f'{data[str(ctx.message.guild.id)]["qa_event"][line]}')
-            await ctx.send(embed=embed)
-
-
-
-    @commands.command()
-    @has_permissions(manage_messages=True)
-    async def startQAevent(self, ctx):
-        id = ''.join(random.choice(string.digits) for _ in range(3))
-        with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
-            data=json.load(f)
-            events = list(data[str(ctx.message.guild.id)]["qa_event"])
-            q = random.choice(events)
-            if str(q).startswith("https://"):
-                embed=discord.Embed(title=f'Q&A Event #{id}', color=65535)
-                embed.set_image(url=q)
+        if choice == 'show':
+            with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
+                data=json.load(f)
+                embed=discord.Embed(title='Q&A Events', color=65535)
+                for line in data[str(ctx.message.guild.id)]["qa_event"]:
+                    embed.add_field(name=f'{line}', value=f'{data[str(ctx.message.guild.id)]["qa_event"][line]}')
                 await ctx.send(embed=embed)
-            else:
-                await ctx.send(embed=discord.Embed(title=f'Q&A Event #{id}', description=f'{q}', color=65535))
+    
 
+        if choice == 'start':
+            event_id = ''.join([random.choice(string.digits) for _ in range(3)])
+            with open(os.path.dirname(__file__) + '\\..\\json\\events.json','r+') as f:
+                data=json.load(f)
+                events = list(data[str(ctx.message.guild.id)]["qa_event"])
+                q = random.choice(events)
+                if str(q).startswith("https://"):
+                    embed=discord.Embed(title=f'Q&A Event #{event_id}', color=65535); embed.set_image(url=q)
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send(embed=discord.Embed(title=f'Q&A Event #{event_id}', description=f'{q}', color=65535))
 
-        
-            while True:
-                try:
-                    msg = await self.client.wait_for("message", check=lambda m: m.channel == ctx.message.channel, timeout=3)
-                    if data[str(ctx.message.guild.id)]["qa_event"][str(q)] in msg.content:
-                        await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} was correct! +20 points', color=65535))
-                        with open(os.path.dirname(__file__) + '\\..\\json\\data.json','r+') as f:
-                            data=json.load(f)
-                            data[str(msg.guild.id)]["users"][str(msg.author.id)]["points"] += 20
-                            self.write("data", data, f)
+                while True:
+                    try:
+                        msg = await self.client.wait_for("message", check=lambda m: m.channel == ctx.message.channel, timeout=10)
+                        if data[str(ctx.message.guild.id)]["qa_event"][str(q)] in msg.content:
+                            await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} was correct! +20 points', color=65535))
+                            with open(os.path.dirname(__file__) + '\\..\\json\\data.json','r+') as f:
+                                data=json.load(f)
+                                data[str(msg.guild.id)]["users"][str(msg.author.id)]["points"] += 20
+                                self.write("data", data, f)
 
-                except asyncio.TimeoutError:
-                    await ctx.send(embed=discord.Embed(title=f'Q&A Event #{id}', description=f'**Correct answer:** {data[str(ctx.message.guild.id)]["qa_event"][str(q)]}', color=65535))
-                    break
-
-
-
-
-
+                    except asyncio.TimeoutError:
+                        await ctx.send(embed=discord.Embed(title=f'Q&A Event #{event_id}', description=f'**Correct answer:** {data[str(ctx.message.guild.id)]["qa_event"][str(q)]}', color=65535))
+                        break
 
 
 
